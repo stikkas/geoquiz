a40adfb376b0462f8f69217a301f9b4ee6e22920
Blagodatskih Serge <stikkas17@gmail.com>
2023-12-24 15:59:59 +0300

    Challenge one is done

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun trueBtnClick(view: View) {
+        Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT)
+            .setBackgroundTint(Color.BLUE)
+            .setTextColor(Color.YELLOW)
+            .show()
+    }
+
+    fun falseBtnClick(view: View) {
+        Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
     }
 }
